137308b975373433d28cbc41584e1f9206b4f242
jayviecondeno20-dev <jayviecondeno20@gmail.com>
2026-04-04 22:17:18 +0800

    Update mailsend.js

--- mailsend.js
+++ mailsend.js
@@ -2,13 +2,16 @@ const nodemailer = require('nodemailer');
 require('dotenv').config(); // Siguraduhing loaded ang environment variables
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // false para sa port 587 (STARTTLS)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
     tls: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
+        requireTLS: true
     },
 });
 
